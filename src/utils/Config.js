1cfbd78b6e6d36f94a7b55a8eb96ca4bb43db60b
hari-prasath14 <139314993+hari-prasath14@users.noreply.github.com>
2025-07-23 08:48:26 -0700

    2025 backend url changed

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -1,3 +1,3 @@
-export const Backend_url = "https://onlineplayerbackend.onrender.com"
+export const Backend_url = "https://my-music-backend-a2q0.onrender.com"
 
 export const Cloudinary_upload_preset = "r1erul06"
